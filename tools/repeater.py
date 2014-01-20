2bb36cefa5088d4be36382745633ab517ec149d2
Michel Pelletier <pelletier.michel@gmail.com>
2014-01-20 07:44:05 -0800

    even more cleanup

--- tools/repeater.py
+++ tools/repeater.py
@@ -4,10 +4,9 @@ import threading
 
 NULL  = bladeRF.ffi.NULL
 
-@bladeRF.callback
-def rx_callback(device, stream, meta_data,
-                samples, num_samples, repeater):
 
+@bladeRF.callback
+def rx_callback(device, stream, meta_data, samples, num_samples, repeater):
     with repeater.samples_available:
         if repeater.rx_idx < 0:
             return NULL
@@ -15,45 +14,36 @@ def rx_callback(device, stream, meta_data,
         repeater.rx_idx += 1
         if repeater.rx_idx >= repeater.num_buffers:
             repeater.rx_idx = 0;
-
         if repeater.num_filled >= 2 * repeater.num_buffers:
             print "RX Overrun encountered. Terminating RX task."
             return NULL
-
         repeater.num_filled += 1
         repeater.samples_available.notify()
     return ret
 
 
 @bladeRF.callback
-def tx_callback(device, stream, meta_data,
-                samples, num_samples, repeater):
+def tx_callback(device, stream, meta_data, samples, num_samples, repeater):
     with repeater.samples_available:
         if repeater.tx_idx < 0:
             return NULL
-
         if repeater.num_filled == 0:
             print "TX underrun encountered. Terminating TX task."
             return NULL
         ret = repeater.rx_stream.buffers[repeater.tx_idx]
         repeater.tx_idx += 1
-
         if repeater.tx_idx >= repeater.num_buffers:
             repeater.tx_idx = 0;
-
         repeater.num_filled -= 1
     return ret
 
 
 class Repeater(object):
 
-    def __init__(self, device_indentifier, config,
-                 num_transfers=16,
-                 samples_per_buffer=8192,
-                 num_buffers=32):
+    def __init__(self, device_indentifier, config, num_transfers=16,
+                 samples_per_buffer=8192, num_buffers=32):
 
         self.device = bladeRF.Device.from_params(device_indentifier, **config)
-
         self.rx_idx = 0
         self.tx_idx = 0
         self.num_filled = 0
@@ -76,7 +66,6 @@ class Repeater(object):
         with self.samples_available:
             while self.num_filled < self.prefill_count and self.tx_idx >= 0:
                 self.samples_available.wait()
-
         self.tx_stream.start()
         i = raw_input('Repeater is running, press enter to exit... ')
         with self.samples_available:
